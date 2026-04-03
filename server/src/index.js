770e3754b4147c0fe39a595737ea95cdf3154650
theTyai <work.mishraashish@gmail.com>
2026-04-03 18:21:52 +0530

    removed old_trsh

--- server/src/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-const connectDB = require('./config/db');
-
-const PORT = process.env.PORT || 5000;
-
-connectDB().then(() => {
-  app.listen(PORT, () => console.log(`LifeOS API running on port ${PORT}`));
-});
